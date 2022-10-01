fc4c7b28da1284d88ec571bdd56ab734babd2dcf
Kingnels <ifeanyiibe2022@gmail.com>
2022-10-01 20:25:28 +0100

    practice

---
+++ src/components/Tasks.js
@@ -0,0 +1,15 @@
+import Task from "../Task"
+
+function Tasks({tasks}) {
+  return (
+    <>
+     {tasks.map((task)=>
+     (<Task key = {task.id} task ={task} />
+
+     ))} 
+     {/* (<h3 key = {task.id}>{task.text}</h3>))}  */}
+    </>
+  )
+}
+
+export default Tasks
